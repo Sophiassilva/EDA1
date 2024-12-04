7f23b4dd60d01bda6a51dece5a68a035c6ccbac0
Sophia Silva <162454522+Sophiassilva@users.noreply.github.com>
2024-12-04 11:24:20 -0300

    Update insertion.c

--- insertion.c
+++ insertion.c
@@ -6,7 +6,44 @@ void swap(int *a, int *b){
     *b = aux;
 }
 
-void insertionO();
+
+void insertionO(int v[], int l, int r){
+    int menor, m;
+    for (int i = l + 1; i <= r; i++)
+    {
+        m = i;
+        menor = v[i];
+        for (int j = i; j > l && menor < v[j - 1]; j--)
+        {
+            v[j] = v[j - 1];
+            m = j - 1;
+        }
+        if (m != i){
+            v[m] = menor;
+        }
+    }
+}
+
+void shell(int v[], int l, int r){
+    int h = 1;
+    int parte = r / 3;
+    while (h < parte){
+        h = h * 3 + 1;
+    }
+
+    while(h > 1){
+        for (int i = h; i <= r; i++){
+            for (int j = i; ((j-h) > (l+h)) && v[j] < v[j-h]; j-= h){
+                swap(&v[j], &v[j - h]);
+            }
+            h = h / 3;
+        }
+    }
+    insertionO(v, l, r);
+}
+
+
+
 
 void insertion(int v[], int l, int r){
     for (int i = l+1; i <= r; i++){
@@ -21,9 +58,9 @@ void insertion(int v[], int l, int r){
 }
 
 int main(){
-    int v[5] = {2, 1, 4, 8, 3};
-    insertion(v, 0, 4);
-    for (int i = 0; i < 5; i++){
+    int v[8] = {-4, -1, 4, 0, 20, 12, 7, 2, };
+    insertion(v, 0, 7);
+    for (int i = 0; i < 8; i++){
         printf("%d ", v[i]);
     }
 }
