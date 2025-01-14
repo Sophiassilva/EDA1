03a99475450df21783436d002caf707d079f4579
Sophiassilva <162454522+Sophiassilva@users.noreply.github.com>
2025-01-14 09:40:44 -0300

    add enfileira

--- filas/enfileira.c
+++ filas/enfileira.c
@@ -2,58 +2,54 @@
 #include <stdlib.h>
 
 typedef struct fila {
-    int *dados;
-    int N, p, u;
+  int *dados;
+  int N, p, u;
 } fila;
 
-int enfileira( fila *f, int x ){
-    if ( f->u+1==f->p || (f->u== f->N && f->p == 0)){
-        f->N*=2;
-        f->dados = realloc(f->dados, f->N*sizeof(int));
+int enfileira (fila *f, int x){
+    if((f->u + 1 == f->p) || (f->p == 0 && (f->u == f->N))){
+        printf("Me barraram </3\n");
+        f->dados = realloc(f->dados, 2 * f->N * sizeof(int));
+        f->N *= 2;
         return 0;
     } else {
+        printf("To entrando\n");
         f->dados[f->u++] = x;
         return 1;
     }
 }
 
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// typedef struct fila {
-//   int *dados;
-//   int N, p, u;
-// } fila;
-
-// int enfileira( fila *f, int x ){
-//     if(f->p == f->u){
-//         if(f->u == f->N){
-//             f->dados[0] = x;
-//             f->u = 0;
-//         }
-//         else{
-//             f->dados[f->p] = x;
-//             f->u = 1;
-//         }
-//     } 
-//     if (f->u != f->p && f->u++ < f->p){
-//         if(f->u < f->N){
-//             f->dados[f->u++] = x;
-//         } else if(0 < f->p){
-//             f->dados[0] = x;
-//             f->u = 1;
-//         }else{
-//             f->N*=2;
-//             f->dados = realloc(f->dados, f->N*sizeof(int));
-//             return 0;
-//         }
-//     }
-//     return 1;
-// }
-
-// // int main(){
-// //     fila unica;
-// //     unica.N = 10;
-// //     unica.p = unica.u = 0;
-// //     enfileira(&unica, 2);
-// // }
+int main(){
+    fila a;
+    a.N = 10;
+    a.dados = calloc(a.N, sizeof(int));
+    if (a.dados == NULL) {
+        printf("Falhou\n");
+        return 1;
+    } else{
+        printf("Ok\n");
+    }
+    a.p = 0;
+    a.u = 0;
+    int i = 0;
+    while(i < 11){
+        int x = enfileira(&a, i);
+        if(x == 0){
+            x = enfileira(&a, i);
+        }
+        i++;
+    }
+    i = 0;
+    while(i < 10){
+        int x = enfileira(&a, i);
+        if(x == 0){
+            x = enfileira(&a, i);
+        }
+        i++;
+    }
+    for (int i = 0; i < 20; i++)
+    {
+        printf("%d\n", a.dados[i]);
+    }
+    free(a.dados);
+}
